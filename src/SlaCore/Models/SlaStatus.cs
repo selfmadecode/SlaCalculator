7541e6a8bee97a71dbd348dbd2ead931c098a99e
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2026-09-05 12:15:58 +0300

    add sla status

---
+++ src/SlaCore/Models/SlaStatus.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SlaCore.Models
+{
+    /// <summary>
+    /// Represents the compliance state of a task against its SLA policy.
+    /// </summary>
+    public enum SlaStatus
+    {
+        /// <summary>The task is within the allowed duration with no warning yet.</summary>
+        OnTrack,
+
+        /// <summary>
+        /// The task has consumed more than <see cref="SlaPolicy.WarningThreshold"/> of its
+        /// allowed duration but has not yet breached.
+        /// </summary>
+        AtRisk,
+
+        /// <summary>The allowed duration has been exceeded and the SLA is breached.</summary>
+        Breached
+    }
+}
